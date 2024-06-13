094e64fa04187dbf85257c5a2c02e54de0aa20be
mika-no-olala <msuranchina@smartex.kz>
2024-06-13 12:24:19 +0500

    changed library name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.smpb05:WebCallSDKAndroid:1.0.0")
+    implementation("com.github.smpb05:WebCallSDK-Android:1.0.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
